23160598064f58f0e6b2030b4254adb0cae83678
mimii020 <imen.abidi@insat.ucar.tn>
2025-11-22 22:45:00 +0100

    fixed ISP issues

---
+++ ISP/src/com/directi/training/isp/exercise_refactored/TimeOutListener.java
@@ -0,0 +1,5 @@
+package com.directi.training.isp.exercise_refactored;
+
+public interface TimeOutListener {
+    public void timeOutCallback();
+}
---
+++ ISP/src/com/directi/training/isp/exercise_refactored/Timer.java
@@ -0,0 +1,20 @@
+package com.directi.training.isp.exercise_refactored;
+
+import java.util.TimerTask;
+
+public class Timer
+{
+    public void register(long timeOut, final TimeOutListener listener)
+    {
+        java.util.Timer timerUtility = new java.util.Timer();
+        timerUtility.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                listener.timeOutCallback();
+            }
+        }, timeOut);
+    }
+}
+
